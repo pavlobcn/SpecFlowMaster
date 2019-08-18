de9d9f257aafba94927c06e6a03d79c29919ab5a
arskiev <arskiev@gmail.com>
2019-08-18 09:59:21 +0200

    Reorganized projects

--- PB.SpecFlowMaster.SpecFlowPlugin/SampleGeneratorPlugin.cs
+++ PB.SpecFlowMaster.SpecFlowPlugin/SampleGeneratorPlugin.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PB.SpecFlowMaster.SpecFlowPlugin;
+using TechTalk.SpecFlow.Generator.CodeDom;
 using TechTalk.SpecFlow.Generator.Plugins;
+using TechTalk.SpecFlow.Generator.UnitTestProvider;
+using TechTalk.SpecFlow.Infrastructure;
 using TechTalk.SpecFlow.UnitTestProvider;
 
+[assembly:GeneratorPlugin(typeof(SampleGeneratorPlugin))]
 namespace PB.SpecFlowMaster.SpecFlowPlugin
 {
     public class SampleGeneratorPlugin : IGeneratorPlugin
@@ -13,7 +13,23 @@ namespace PB.SpecFlowMaster.SpecFlowPlugin
         public void Initialize(GeneratorPluginEvents generatorPluginEvents, GeneratorPluginParameters generatorPluginParameters,
             UnitTestProviderConfiguration unitTestProviderConfiguration)
         {
-            unitTestProviderConfiguration.UseUnitTestProvider("MSTest");
+            System.Diagnostics.Debugger.Launch();
+            unitTestProviderConfiguration.UseUnitTestProvider("MyProvider");
+            //generatorPluginEvents.CustomizeDependencies += GeneratorPluginEvents_CustomizeDependencies;
+            generatorPluginEvents.RegisterDependencies += GeneratorPluginEvents_CustomizeDependencies;
+        }
+
+        private void GeneratorPluginEvents_CustomizeDependencies(object sender, RegisterDependenciesEventArgs e)
+        {
+            System.Diagnostics.Debugger.Launch();
+            e.ObjectContainer.RegisterTypeAs<MyGeneratorProvider, IUnitTestGeneratorProvider>("MyProvider");
+        }
+    }
+
+    public class MyGeneratorProvider : MsTestV2GeneratorProvider
+    {
+        public MyGeneratorProvider(CodeDomHelper codeDomHelper) : base(codeDomHelper)
+        {
             System.Diagnostics.Debugger.Launch();
         }
     }
