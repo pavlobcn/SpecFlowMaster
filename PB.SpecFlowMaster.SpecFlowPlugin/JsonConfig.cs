3a0c69d73a5f613fd30ba498e3d3633a344e9a7c
arskiev <arskiev@gmail.com>
2019-08-18 13:21:58 +0200

    Added specflowmaster configuration

---
+++ PB.SpecFlowMaster.SpecFlowPlugin/JsonConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.Serialization;
+
+namespace PB.SpecFlowMaster.SpecFlowPlugin
+{
+    public class JsonConfig
+    {
+        private const string DefaultUnitTestProvider = "nunit";
+
+        [DataMember(Name = "unitTestProvider")]
+        [DefaultValue(DefaultUnitTestProvider)]
+        public string UnitTestProvider { get; set; }
+
+        public static JsonConfig GetDefault()
+        {
+            return new JsonConfig { UnitTestProvider = DefaultUnitTestProvider };
+        }
+    }
+}
---
+++ PB.SpecFlowMaster.SpecFlowPlugin/MasterGeneratorProvider.cs
@@ -0,0 +1,118 @@
+﻿using System.CodeDom;
+using System.Collections.Generic;
+using BoDi;
+using TechTalk.SpecFlow.Generator;
+using TechTalk.SpecFlow.Generator.UnitTestProvider;
+
+namespace PB.SpecFlowMaster.SpecFlowPlugin
+{
+    public class MasterGeneratorProvider : IUnitTestGeneratorProvider
+    {
+        public const string Name = "SpecflowMaster";
+
+        private readonly IObjectContainer _container;
+        private IUnitTestGeneratorProvider _baseUnitTestGeneratorProvider;
+
+        public MasterGeneratorProvider(IObjectContainer container)
+        {
+            _container = container;
+            System.Diagnostics.Debugger.Launch();
+        }
+
+        public IUnitTestGeneratorProvider BaseUnitTestGeneratorProvider
+        {
+            get
+            {
+                if (_baseUnitTestGeneratorProvider == null)
+                {
+                    JsonConfig config = _container.Resolve<JsonConfig>();
+                    _baseUnitTestGeneratorProvider = _container.Resolve<IUnitTestGeneratorProvider>(config.UnitTestProvider);
+                }
+                return _baseUnitTestGeneratorProvider;
+            }
+        }
+
+        public UnitTestGeneratorTraits GetTraits()
+        {
+            return BaseUnitTestGeneratorProvider.GetTraits();
+        }
+
+        public void SetTestClass(TestClassGenerationContext generationContext, string featureTitle, string featureDescription)
+        {
+            BaseUnitTestGeneratorProvider.SetTestClass(generationContext, featureTitle, featureDescription);
+        }
+
+        public void SetTestClassCategories(TestClassGenerationContext generationContext, IEnumerable<string> featureCategories)
+        {
+            BaseUnitTestGeneratorProvider.SetTestClassCategories(generationContext, featureCategories);
+        }
+
+        public void SetTestClassIgnore(TestClassGenerationContext generationContext)
+        {
+            BaseUnitTestGeneratorProvider.SetTestClassIgnore(generationContext);
+        }
+
+        public void FinalizeTestClass(TestClassGenerationContext generationContext)
+        {
+            BaseUnitTestGeneratorProvider.FinalizeTestClass(generationContext);
+        }
+
+        public void SetTestClassParallelize(TestClassGenerationContext generationContext)
+        {
+            BaseUnitTestGeneratorProvider.SetTestClassParallelize(generationContext);
+        }
+
+        public void SetTestClassInitializeMethod(TestClassGenerationContext generationContext)
+        {
+            BaseUnitTestGeneratorProvider.SetTestClassInitializeMethod(generationContext);
+        }
+
+        public void SetTestClassCleanupMethod(TestClassGenerationContext generationContext)
+        {
+            BaseUnitTestGeneratorProvider.SetTestClassCleanupMethod(generationContext);
+        }
+
+        public void SetTestInitializeMethod(TestClassGenerationContext generationContext)
+        {
+            BaseUnitTestGeneratorProvider.SetTestInitializeMethod(generationContext);
+        }
+
+        public void SetTestCleanupMethod(TestClassGenerationContext generationContext)
+        {
+            BaseUnitTestGeneratorProvider.SetTestCleanupMethod(generationContext);
+        }
+
+        public void SetTestMethod(TestClassGenerationContext generationContext, CodeMemberMethod testMethod, string friendlyTestName)
+        {
+            BaseUnitTestGeneratorProvider.SetTestMethod(generationContext, testMethod, friendlyTestName);
+        }
+
+        public void SetTestMethodCategories(TestClassGenerationContext generationContext, CodeMemberMethod testMethod,
+            IEnumerable<string> scenarioCategories)
+        {
+            BaseUnitTestGeneratorProvider.SetTestMethodCategories(generationContext, testMethod, scenarioCategories);
+        }
+
+        public void SetTestMethodIgnore(TestClassGenerationContext generationContext, CodeMemberMethod testMethod)
+        {
+            BaseUnitTestGeneratorProvider.SetTestMethodIgnore(generationContext, testMethod);
+        }
+
+        public void SetRowTest(TestClassGenerationContext generationContext, CodeMemberMethod testMethod, string scenarioTitle)
+        {
+            BaseUnitTestGeneratorProvider.SetRowTest(generationContext, testMethod, scenarioTitle);
+        }
+
+        public void SetRow(TestClassGenerationContext generationContext, CodeMemberMethod testMethod, IEnumerable<string> arguments,
+            IEnumerable<string> tags, bool isIgnored)
+        {
+            BaseUnitTestGeneratorProvider.SetRow(generationContext, testMethod, arguments, tags, isIgnored);
+        }
+
+        public void SetTestMethodAsRow(TestClassGenerationContext generationContext, CodeMemberMethod testMethod, string scenarioTitle,
+            string exampleSetName, string variantName, IEnumerable<KeyValuePair<string, string>> arguments)
+        {
+            BaseUnitTestGeneratorProvider.SetTestMethodAsRow(generationContext, testMethod, scenarioTitle, exampleSetName, variantName, arguments);
+        }
+    }
+}
