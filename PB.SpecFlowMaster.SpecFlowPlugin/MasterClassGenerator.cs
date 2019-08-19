33dfffbccfafadbaa1291b4e6ebba044539bdbeb
Pavlo Biriukov <pavlo.biriukov@weareadaptive.com>
2019-08-19 11:41:44 +0200

    Added MasterClassGenerator

--- PB.SpecFlowMaster.SpecFlowPlugin/MasterClassGenerator.cs
+++ PB.SpecFlowMaster.SpecFlowPlugin/MasterClassGenerator.cs
@@ -2,6 +2,7 @@
 using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using Gherkin.Ast;
@@ -33,6 +34,33 @@ namespace PB.SpecFlowMaster.SpecFlowPlugin
                     testClass.Members.Add(testMethod);
 
                     testMethod.Name = "TestLine" + step.Location.Line;
+                    testMethod.Statements.Add(new CodeVariableDeclarationStatement(typeof(bool),
+                        "expectedExceptionOccured", new CodePrimitiveExpression(false)));
+                    var tryCatchStatement = new CodeTryCatchFinallyStatement
+                    {
+                        TryStatements = { },
+                        CatchClauses =
+                        {
+                            new CodeCatchClause()
+                            {
+                                Statements =
+                                {
+                                    new CodeAssignStatement(
+                                        new CodeVariableReferenceExpression("expectedExceptionOccured"),
+                                        new CodePrimitiveExpression(true))
+                                }
+                            }
+                        }
+                    };
+                    testMethod.Statements.Add(tryCatchStatement);
+                    var exceptionValidationStatement = new CodeConditionStatement
+                    {
+                        Condition = new UnaryExpression
+                        {
+                            
+                        }
+                    }
+                    testMethod.Statements.Add(exceptionValidationStatement);
                 }
             }
         }
@@ -41,6 +69,24 @@ namespace PB.SpecFlowMaster.SpecFlowPlugin
         {
             var testClass = new CodeTypeDeclaration("MasterTests");
             return testClass;
+        }
+
+        private void TestTemplate()
+        {
+            bool expectedExceptionOccured = false;
+            try
+            {
+
+            }
+            catch
+            {
+                expectedExceptionOccured = true;
+            }
+
+            if (!expectedExceptionOccured)
+            {
+                throw new Exception("Suspicious statement at line #");
+            }
         }
     }
 }
