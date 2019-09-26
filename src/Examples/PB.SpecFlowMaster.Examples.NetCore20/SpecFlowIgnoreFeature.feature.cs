2365532335ee7ba8c8c450e93f4e46d41655ef70
arskiev <arskiev@gmail.com>
2019-09-26 12:30:27 +0200

    Fixed netcore examples

--- src/Examples/PB.SpecFlowMaster.Examples.NetCore20/SpecFlowIgnoreFeature.feature.cs
+++ src/Examples/PB.SpecFlowMaster.Examples.NetCore20/SpecFlowIgnoreFeature.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace PB.SpecFlowMaster.Examples.NetStandard20
+namespace PB.SpecFlowMaster.Examples.NetCore20
 {
     using TechTalk.SpecFlow;
     
